fdc2f37d8d5a1a001072781a32aea34ff0e3b7b2
Jonathan <johjo@ytreza.org>
2025-07-16 16:14:43 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:toto.spec.ts

--- toto.spec.ts
+++ toto.spec.ts
@@ -1,7 +1,37 @@
-import {describe, it, expect} from "vitest";
+import {describe, expect, it} from "vitest";
 
-describe("toto", () => {
-    it("should pass", () => {
-        expect(true).toBe(true);
+describe("US-1 - Afficher deux tâche en comparaison", () => {
+    it("US-1-AC-1 : Sans priorité, on affiche les deux premières tâches", () => {
+        // GIVEN
+        const initialTasks = [
+            "Envoyer un mail à la direction",
+            "Nettoyer mon bureau",
+            "Payer la facture d'électricité",
+            "Corriger le bug qui fait perdre de l'argent au client"];
+
+        // WHEN
+        let tasksCompared = initialTasks.slice(0, 2);
+        // Quand
+        // on affiche la comparaison
+
+        // THEN
+        expect(tasksCompared).toStrictEqual(["Envoyer un mail à la direction", "Nettoyer mon bureau"]);
+    });
+
+    it("US-1-AC-2 : Sans priorité, on affiche les deux premières tâches", () => {
+        // GIVEN
+        const initialTasks = [
+            "Envoyer un mail à la direction",
+            "Nettoyer mon bureau",
+            "Payer la facture d'électricité",
+            "Corriger le bug qui fait perdre de l'argent au client"];
+
+        // WHEN
+        let tasksCompared = initialTasks.slice(0, 2);
+        // Quand
+        // on affiche la comparaison
+
+        // THEN
+        expect(tasksCompared).toStrictEqual(["Envoyer un mail à la direction", "Nettoyer mon bureau"]);
     });
 })
