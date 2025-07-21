3fb11c04acda73dc8eb50f41be6ba9d334b55856
Jonathan <johjo@ytreza.org>
2025-07-21 15:36:29 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:node_modules/.vite/vitest/da39a3ee5e6b4b0d3255bfef95601890afd80709/results.json

--- toto.spec.ts
+++ toto.spec.ts
@@ -1,6 +1,21 @@
 import {describe, expect, it} from "vitest";
 
 class TaskFvpQuery {
+    public repo: TaskRepository;
+
+    query() {
+        if (this.repo.initialTasks[1].status === "new") {
+            return [
+                this.repo.initialTasks[0],
+                this.repo.initialTasks[1]
+            ];
+        }
+
+        return [
+            this.repo.initialTasks[0],
+            this.repo.initialTasks[2]
+        ];
+    }
 }
 
 type Task = {
@@ -8,48 +23,82 @@ type Task = {
     status: "new" | "next" | "later";
 }
 
+class TaskRepository {
+    initialTasks: Task[];
+
+    feed(tasks: Task[]) {
+        this.initialTasks = tasks;
+    }
+}
+
 describe("US-1 - Afficher deux tâche en comparaison", () => {
     it("US-1-AC-1 : Sans priorité, on affiche les deux premières tâches", () => {
         // GIVEN
-        const initialTasks = [
+        const sut = new TaskFvpQuery();
+        sut.repo = new TaskRepository();
+        sut.repo.feed([
             {name: "Envoyer un mail à la direction", status: "new"},
             {name: "Nettoyer mon bureau", status: "new"},
+            {name: "Payer la facture d'électricité", status: "new"},
+            {name: "Corriger le bug qui fait perdre de l'argent au client", status: "new"},
+        ])
+
+
+        // WHEN
+        let tasksCompared = sut.query()
+        // Quand
+        // on affiche la comparaison
+
+        // THEN
+        expect(tasksCompared).toStrictEqual([{
+            name: "Envoyer un mail à la direction",
+            status: "new"
+        }, {name: "Nettoyer mon bureau", status: "new"}]);
+    });
+
+    it("US-1-AC-2 : Avec une tâche priorisée (next) et une tâche comparée (later), on affiche la tâche priorisée et la première tâche non priorisée et non comparée", () => {
+        // GIVEN
+        const sut = new TaskFvpQuery();
+        sut.repo = new TaskRepository()
+        sut.repo.initialTasks = [
+            {name: "Envoyer un mail à la direction", status: "next"},
+            {name: "Nettoyer mon bureau", status: "later"},
             {name: "Payer la facture d'électricité", status: "new"},
             {name: "Corriger le bug qui fait perdre de l'argent au client", status: "new"},
         ];
 
         // WHEN
-        let tasksCompared = initialTasks.slice(0, 2);
+        let tasksCompared = sut.query()
         // Quand
         // on affiche la comparaison
 
         // THEN
-        expect(tasksCompared).toStrictEqual([{name: "Envoyer un mail à la direction", status: "new"}, {name: "Nettoyer mon bureau", status: "new"}]);
+        expect(tasksCompared).toStrictEqual([{
+            name: "Envoyer un mail à la direction",
+            status: "next"
+        }, {name: "Payer la facture d'électricité", status: "new"}]);
     });
 
-   it("US-1-AC-2 : Sans priorité, on affiche les deux premières tâches", () => {
-       // GIVEN
+    it("US-1-AC-2 : Avec une tâche priorisée (next) et une tâche comparée (later), on affiche la tâche priorisée et la première tâche non priorisée et non comparée", () => {
+        // GIVEN
+        const sut = new TaskFvpQuery();
+        sut.repo = new TaskRepository()
+        sut.repo.initialTasks = [
+            {name: "Envoyer un mail à la direction", status: "next"},
+            {name: "Nettoyer mon bureau", status: "later"},
+            {name: "Payer la facture d'électricité", status: "new"},
+            {name: "Corriger le bug qui fait perdre de l'argent au client", status: "new"},
+        ];
 
+        // WHEN
+        let tasksCompared = sut.query()
+        // Quand
+        // on affiche la comparaison
 
-       const sut = new TaskFvpQuery();
-
-
-       const initialTasks: [Task] = [
-           {name: "Envoyer un mail à la direction", status: "next"},
-           {name: "Nettoyer mon bureau", status: "later"},
-           {name: "Payer la facture d'électricité", status: "new"},
-           {name: "Corriger le bug qui fait perdre de l'argent au client", status: "new"},
-       ];
-
-       // WHEN
-       let tasksCompared = [
-              initialTasks[0],
-              initialTasks[2]
-       ];
-       // Quand
-       // on affiche la comparaison
-
-       // THEN
-       expect(tasksCompared).toStrictEqual([{name: "Envoyer un mail à la direction", status: "next"}, {name: "Payer la facture d'électricité", status: "new"}]);
+        // THEN
+        expect(tasksCompared).toStrictEqual([{
+            name: "Envoyer un mail à la direction",
+            status: "next"
+        }, {name: "Payer la facture d'électricité", status: "new"}]);
     });
 })
